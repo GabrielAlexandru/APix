95b45102b2fb69ea3d65662a9d87bb59fc1a9214
Cazacu Vlad-Gabriel <gabrielcazacu96@gmail.com>
2017-11-27 22:40:27 +0200

    javascript for forms, errors, help text

--- static/registration/login.js
+++ static/registration/login.js
@@ -3,5 +3,13 @@ window.onload = function () {
     var inputs = document.querySelectorAll("input");
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].placeholder = inputs[i].name;
+        inputs[i].onfocus = function () {
+            this.nextElementSibling.style.display = "block";
+        };
+        inputs[i].onblur = function () {
+            this.nextElementSibling.style.display = "none";
+        };
     }
-}
+
+    document.getElementsByTagName("input")[0].blur()
+};
