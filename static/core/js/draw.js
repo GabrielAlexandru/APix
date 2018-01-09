3e83b40b723ac27781a742570274d635a08d0f7d
AlexandruGabriel <gabriel.alexandru@info.uaic.ro>
2018-01-09 18:04:34 +0200

    some things

--- static/core/js/draw.js
+++ static/core/js/draw.js
@@ -46,6 +46,7 @@ function color(obj) {
     }
     picker.addEventListener("input", function () {
         x = picker.value;
+        document.styleSheets[2].insertRule("input[type=range]::-webkit-slider-thumb { background: " + x + "; }", document.styleSheets[2].cssRules.length);
     }, false);
     obj.className += "selected";
     switch (obj.id) {
@@ -144,4 +145,44 @@ function findxy(res, e) {
             draw();
         }
     }
+}
+
+function findPos(obj) {
+    var curleft = 0, curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curleft += obj.offsetLeft;
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curleft, y: curtop };
+    }
+    return undefined;
+}
+
+function rgbToHex(r, g, b) {
+    if (r > 255 || g > 255 || b > 255)
+        throw "Invalid color component";
+    return ((r << 16) | (g << 8) | b).toString(16);
+}
+
+function pick() {
+    var picker2 = document.getElementById("color-picker");
+    if(picker2.className != "picker-on"){
+        canvas.style.cursor = "crosshair";
+        picker2.className = "picker-on";
+    } else {
+        canvas.style.cursor = "auto";
+        picker2.className = "";
+    }
+    var context = canvas.getContext('2d');
+    canvas.addEventListener("mousemove", function(e) {
+        var pos = findPos(this);
+        var x = e.pageX - pos.x;
+        var y = e.pageY - pos.y;
+        var coord = "x=" + x + ", y=" + y;
+        // var c = this.getContext('2d');
+        var p = context.getImageData(x, y, 1, 1).data;
+        var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
+        console.log(p);
+    }, false);
 }
