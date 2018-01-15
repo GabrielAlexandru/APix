e50cf5347a2e82d9b117ae9d5ad250e70cee7d74
Cazacu Vlad-Gabriel <gabrielcazacu96@gmail.com>
2018-01-15 22:10:54 +0200

    fixed sum sockets and bugs

--- static/core/js/channel.js
+++ static/core/js/channel.js
@@ -1,20 +1,26 @@
 var Channel = function () {
-        this.socket = null;
-        this.ball = null;
-        this.maxY = null;
-        this.maxX = null;
+    this.socket = null;
+    this.tick = false;
+    this.ball = null;
+    this.maxY = null;
+    this.maxX = null;
 
-        this.initSocket = function (target, device) {
+    this.initSocket = function (target, device) {
 
-            this.socket = new WebSocket("ws://" + window.location.host + "/" + target + "/");
+        this.socket = new WebSocket("ws://" + window.location.host + "/" + target + "/");
 
-            this.socket.device = device;
-            this.socket.target = target;
+        this.socket.device = device;
+        this.socket.target = target;
 
-            this.socket.onmessage = function (e) {
-                var message_json = JSON.parse(e.data);
-                if (device === "desktop") {
+        window.setInterval(function () {
+            tick = true;
+        }, 1000);
 
+        this.socket.onmessage = function (e) {
+            var message_json = JSON.parse(e.data);
+            if (device === "desktop") {
+
+                if (username !== message_json['username']) {
                     if (message_json['type'] === "command") {
                         var text = message_json['text'];
                         //text = [prevX, prevY, currX, currY, x, y];
@@ -33,81 +39,101 @@ var Channel = function () {
                         };
                         img.src = message_json['text']
                     }
-                    if (message_json['type'] === "remote") {
-                        console.log("remote");
-                        var text = message_json['text'];
-                        // ball = document.getElementById('ball');
-                        // maxX = canvas.clientWidth - ball.clientWidth;
-                        // maxY = canvas.clientHeight - ball.clientHeight;
-                        var x = text[0];
-                        var y = text[1];
-                        if (x > 90) {
-                            x = 90
-                        }
-                        if (x < -90) {
-                            x = -90
-                        }
-                        x += 90;
-                        y += 90;
-                        // ball.style.top = (maxX * x / 180 - 10) + "px";
-                        // ball.style.left = (maxY * y / 180 - 10) + "px";
+
+                    if (message_json['type'] === "canvas-clear") {
+                        drawHelper.ctx.clearRect(0, 0, drawHelper.canvas.width, drawHelper.canvas.height);
                     }
                 }
-            };
-
-            this.socket.onopen = function () {
-                var msg = {
-                    type: "message",
-                    text: "Logged in",
-                    room: username
-                };
-                this.send(JSON.stringify(msg));
-            };
-
-            this.socket.onclose = function () {
-                alert("Oups, socket closed, need to fix, refresh page 4 ez fix");
+                if (message_json['type'] === "remote") {
+                    console.log("remote");
+                    var text = message_json['text'];
+                    // ball = document.getElementById('ball');
+                    // maxX = canvas.clientWidth - ball.clientWidth;
+                    // maxY = canvas.clientHeight - ball.clientHeight;
+                    var x = text[0];
+                    var y = text[1];
+                    if (x > 90) {
+                        x = 90
+                    }
+                    if (x < -90) {
+                        x = -90
+                    }
+                    x += 90;
+                    y += 90;
+                    // ball.style.top = (maxX * x / 180 - 10) + "px";
+                    // ball.style.left = (maxY * y / 180 - 10) + "px";
+                }
             }
         };
 
-        this.sendCanvasCopy = function (canvas) {
-            if (this.socket.readyState === 1) {
-                var dataURL = canvas.toDataURL();
-                var msg = {
-                    type: "canvas",
-                    text: dataURL,
-                    room: username
-                };
+        this.socket.onopen = function () {
+            var msg = {
+                type: "message",
+                text: "Logged in",
+                room: username
+            };
+            this.send(JSON.stringify(msg));
+        };
 
+        this.socket.onclose = function () {
+            console.log("Websocket down, reinitializing...");
+            this.initSocket(this.socket.target, this.socket.device);
+        }.bind(this);
+    }.bind(this);
+
+    this.sendCanvasCopy = function (canvas) {
+        if (this.socket.readyState === 1) {
+            var dataURL = canvas.toDataURL();
+            var msg = {
+                type: "canvas",
+                text: dataURL,
+                room: username
+            };
+
+            this.socket.send(JSON.stringify(msg));
+        }
+    }.bind(this);
+
+    this.sendClearCanvas = function () {
+        if (this.socket.readyState === 1) {
+            var msg = {
+                type: "canvas-clear",
+                text: "command",
+                room: username
+            };
+
+            this.socket.send(JSON.stringify(msg));
+        }
+    }.bind(this);
+
+    this.sendRemotePosition = function (event) {
+        if (this.socket.readyState === 1) {
+            var x = event.beta;
+            var y = event.gamma;
+            var text = [x, y];
+
+            var msg = {
+                type: "remote",
+                text: text,
+                room: username
+            };
+            if (tic)
                 this.socket.send(JSON.stringify(msg));
-            }
-        }.bind(this);
+        }
+    }.bind(this);
 
-        this.sendRemotePosition = function (event) {
-            if (this.socket.readyState === 1) {
-                var x = event.beta;
-                var y = event.gamma;
-                var text = [x, y];
+    this.sendInstructions = function (prevX, prevY, currX, currY, colorX, pencilSize) {
+        if (this.socket.readyState === 1 && this.tick === true) {
 
-                var msg = {
-                    type: "remote",
-                    text: text,
-                    room: username
-                };
-                if (tic)
-                    this.socket.send(JSON.stringify(msg));
-            }
-        }.bind(this);
+            text = [prevX, prevY, currX, currY, colorX, pencilSize];
+            var msg = {
+                type: "command",
+                text: text,
+                room: username
+            };
+            this.socket.send(JSON.stringify(msg));
 
-        this.sendInstructions = function (prevX, prevY, currX, currY, colorX, pencilSize) {
-            if (this.socket.readyState === 1) {
-
-                text = [prevX, prevY, currX, currY, colorX, pencilSize];
-                var msg = {
-                    type: "command",
-                    text: text,
-                    room: username
-                };
-                this.socket.send(JSON.stringify(msg));
-            }
-        }.bind(this);
-    };
+            this.tick = false;
+        }
+    }.bind(this);
+};
